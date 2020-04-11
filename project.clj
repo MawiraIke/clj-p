294eb61f5d5c677ae79e79f727d54cd495f02db7
Ike Mawira <mawiraike@gmail.com>
2020-04-11 19:00:15 +0300

    Update CSS compile commands for automatic compilation
    Signed-off-by: Ike Mawira <mawiraike@gmail.com>

--- project.clj
+++ project.clj
@@ -52,8 +52,8 @@
   :profiles
   {:dev
    {:dependencies [[binaryage/devtools "0.9.11"]
-                   [day8.re-frame/re-frame-10x "0.4.4"]
                    [day8.re-frame/tracing "0.5.3"]]}
 
    :prod { :dependencies [[day8.re-frame/tracing-stubs "0.5.3"]]}
-   })
+   }
+  :prep-tasks [["garden" "once"]])
