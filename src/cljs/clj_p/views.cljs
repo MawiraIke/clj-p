82857678e238910c46e1aa4636f779966597f322
Ike Mawira <mawiraike@gmail.com>
2020-04-12 00:35:37 +0300

    Final version, April 2020
    Signed-off-by: Ike Mawira <mawiraike@gmail.com>

--- src/cljs/clj_p/views.cljs
+++ src/cljs/clj_p/views.cljs
@@ -22,36 +22,50 @@
 ;;   ])
 
 ;; Bootstrap navigation bar, needs formatting
-(defn navigation-bar
-  []
-  [:nav {:class "navbar navbar-expand-lg navbar-dark bg-dark"}
-   [:a {:class "navbar-brand" :href "/#"} "Home"]
-   [:button {:class         "navbar-toggler"
-             :type          "button"
-             :data-toggle   "collapse"
-             :data-target   "#navbarNav"
-             :aria-controls "navbarNav"
-             :aria-expanded "false"
-             :aria-label    "Toggle navigation"} [:span {:class "navbar-toggler-icon"}]]
-   [:div {:class "collapse navbar-collapse"
-          :id    "navbarNav"}
-    [:ul {:class "navbar-nav my-2 my-lg-0"}
-     ;;[:li {:class "nav-item active"}
-     ;; [:a {:class "nav-link" :href "/#"} "Home" [:span {:class "sr-only"} "(current)"]]]
-     ;;[:li {:class "nav-item"}
-     ;; [:a {:class "nav-link" :href "/#spacerr"} "CV"]]
-     ]]])
+;;(defn navigation-bar
+;;  []
+;;  [:nav {:class "navbar navbar-expand-lg navbar-dark bg-dark"}
+;;   [:a {:class "navbar-brand" :href "/#"} "Home"]
+;;   [:button {:class         "navbar-toggler"
+;;             :type          "button"
+;;             :data-toggle   "collapse"
+;;             :data-target   "#navbarNav"
+;;             :aria-controls "navbarNav"
+;;             :aria-expanded "false"
+;;             :aria-label    "Toggle navigation"} [:span {:class "navbar-toggler-icon"}]]
+;;   [:div {:class "collapse navbar-collapse"
+;;          :id    "navbarNav"}
+;;    [:ul {:class "navbar-nav my-2 my-lg-0"}
+;;     ;;[:li {:class "nav-item active"}
+;;     ;; [:a {:class "nav-link" :href "/#"} "Home" [:span {:class "sr-only"} "(current)"]]]
+;;     ;;[:li {:class "nav-item"}
+;;     ;; [:a {:class "nav-link" :href "/#spacerr"} "CV"]]
+;;     ]]])
 
 (defn image-section
   []
-  [:img {:src "images/code.jpeg"
-         :alt "Computer"}])
+  [re-com/v-box
+   :height "50%"
+   :children [[:img {:src "images/code.jpeg"
+                     :alt "Computer"}]]])
 
 (defn text-overlay
   []
-  [:div {:class "center-text"}
-   [:p "Ike Mawira"]
-   [:p "CS Student, Software Developer."]])
+  [re-com/h-box
+   :style {:position  "absolute"
+           :top       "50%"
+           :left      "10%"
+           :transform "translate(-50%, -50%)"}
+   :children [[re-com/v-box
+               :children [[re-com/title
+                           :style {:color "white"}
+                           :level :level2
+                           :label "Ike Mawira"]
+                          [re-com/title
+                           :style {:color "white"}
+                           :level :level3
+                           :label "CS Student, Software Developer."]]]]]
+  )
 
 (defn space-add
   []
@@ -61,9 +75,9 @@
 (defn space-add-with-pdf
   []
   [:div {:class "spacer-with-pdf"
-         :id "spacerr"}
+         :id    "spacerr"}
    [:a {:class "resume-link"
-        :href  "/docs/resume.pdf"} "Click here for resume as PDF"]])
+        :href  "/docs/Ike Mawira.pdf"} "Click here for resume as PDF"]])
 
 (defn minor-space
   []
@@ -72,33 +86,44 @@
 (defn copyright
   []
   [:div {:class "copyright-contain"}
-   [:p "Ike Mawira 2019"]])
+   [:p "Ike Mawira 2020"]])
 
 (defn social-links
   []
-  [:container {:class "row"}
-   [:a {:class "col-md-2" :href "mailto:mawiraike@gmail.com"} "EMAIL"]
-   [:a {:class "col-md-2" :href "https://www.linkedin.com/in/ike-mawira-706974163"} "LINKEDIN"]
-   [:a {:class "col-md-2" :href "https://github.com/MawiraIke"} "GITHUB"]
-   [:a {:class "col-md-2" :href "https://stackoverflow.com/users/10081740/ike-mawira"} "STACK OVERFLOW"]
-   [:a {:class "col-md-2" :href "https://twitter.com/ike__dev"} "TWITTER"]
-   [:a {:class "col-md-2" :href "https://www.buymeacoffee.com/ikeM"} "BUY ME A COFFEE"]])
+  [re-com/h-box
+   :justify :between
+   :children [[:a {:class "col-md-2" :href "mailto:mawiraike@gmail.com"} [re-com/title
+                                                                          :style {:color "white"}
+                                                                          :label "EMAIL"]]
+              [:a {:class "col-md-2" :href "https://www.linkedin.com/in/ike-mawira-706974163"} [re-com/title
+                                                                                                :style {:color "white"}
+                                                                                                :label "LINKED-IN"]]
+              [:a {:class "col-md-2" :href "https://github.com/MawiraIke"} [re-com/title
+                                                                            :style {:color "white"}
+                                                                            :label "GITHUB"]]
+              [:a {:class "col-md-2" :href "https://stackoverflow.com/users/10081740/ike-mawira"} [re-com/title
+                                                                                                   :style {:color "white"}
+                                                                                                   :label "STACK OVERFLOW"]]
+              [:a {:class "col-md-2" :href "https://twitter.com/ike__dev"} [re-com/title
+                                                                            :style {:color "white"}
+                                                                            :label "TWITTER"]]
+              [:a {:class "col-md-2" :href "https://www.buymeacoffee.com/ikeM"} [re-com/title
+                                                                                 :style {:color "white"}
+                                                                                 :label "BUY ME A COFFEE"]]]])
 
 (defn main-panel []
-  [:div
-   [navigation-bar]
-   [:div {:class "contain"}
-    [image-section]
-    [text-overlay]]
-   ;; leave this here cause there is some undesirable space after the image if this is not here
-   [re-com/v-box
-    :width "100%"
-    :children [[space-add]]]
-   [space-add-with-pdf]
-   [:footer {:class "footer-c"}
-    [minor-space]
-    [copyright]
-    [minor-space]
-    [social-links]
-    [minor-space]
-    [minor-space]]])
+  [re-com/v-box
+   :children [[:div {:class "contain"}
+               [image-section]
+               [text-overlay]]
+              ;; leave this here cause there is some undesirable space after the image if this is not here
+              [re-com/v-box
+               :width "100%"
+               :children [[space-add]]]
+              [space-add-with-pdf]
+              [:footer {:class "footer-c"}
+               [minor-space]
+               [social-links]
+               [minor-space]]]
+   ;;[navigation-bar]
+   ])
